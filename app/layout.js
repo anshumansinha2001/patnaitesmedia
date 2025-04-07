4f9a44d865835145dc570bcc7de67b48f23679be
Anshuman Sinha <128638190+anshumansinha2001@users.noreply.github.com>
2025-04-07 16:10:15 +0530

    removed webmenifist

--- app/layout.js
+++ app/layout.js
@@ -63,7 +63,6 @@ export default function RootLayout({ children }) {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
         {/* Google Search Console */}
         <meta
           name="google-site-verification"
